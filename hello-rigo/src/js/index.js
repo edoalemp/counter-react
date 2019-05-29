3d488576bffb3f513b3ff41e4959085b02f22871
edoalemp <eduardo.munoz.p@gmail.com>
2019-05-29 16:32:02 +0000

    contador funcionando

--- hello-rigo/src/js/index.js
+++ hello-rigo/src/js/index.js
@@ -10,13 +10,8 @@ import "bootstrap";
 import "../styles/index.scss";
 
 //import your own components
-//import { secondsCounter } from "./component/home.js";
-
 let seconds = 0;
-setInterval((seconds = seconds + 1), 1000);
-
 const SecondsCounter = props => {
-	//setInterval((seconds = seconds + 1), 1000);
 	return (
 		<div className="text-center mt-5">
 			<h1>{props.secs}</h1>
@@ -29,7 +24,16 @@ SecondsCounter.propTypes = {
 };
 
 //render your react application
-ReactDOM.render(
-	<SecondsCounter secs={seconds} />,
-	document.querySelector("#app")
-);
+
+setInterval(function() {
+	ReactDOM.render(
+		<SecondsCounter secs={seconds} />,
+		document.querySelector("#app")
+	);
+	seconds++;
+}, 1000);
+
+//ReactDOM.render(
+//	<SecondsCounter secs={seconds} />,
+//	document.querySelector("#app")
+//);
